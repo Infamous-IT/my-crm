ac2781acce21740c40310923d2e5f0817bb55723
infamous-it <it.dev.23@gmail.com>
2024-09-05 09:34:10 +0300

    Initial commit

---
+++ src/test/java/com/smart_solution/my_crm/MyCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart_solution.my_crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
